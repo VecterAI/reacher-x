083a6fb925b35a5ab4eb1a7adf1b153ef23f6ec5
Muhammad Salman Farooq ✓ <creativecoder.crco@gmail.com>
2025-11-30 22:57:11 +0500

    feat: add module index files and new hooks for better organization
    - Add index files for cleaner module exports
    - Add new hooks: useOgPreview, useUrlDescription, useWorkspace
    - Add X (Twitter) post route structure
    - Improve code organization and maintainability

---
+++ features/landing/ui/components/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Barrel exports for landing UI components
+ */
+
+export { AvatarStack } from "./AvatarStack";
+export { AvatarStackSkeleton } from "./AvatarStackSkeleton";
+export { FigureVideo } from "./FigureVideo";
+export { Footer } from "./Footer";
+export { Header } from "./Header";
+export { LandingAutoPlayProvider } from "./LandingAutoPlayProvider";
+export { default as LandingVideoPlayer } from "./LandingVideoPlayer";
+export { LinkWrapper } from "./LinkWrapper";
+export { NavLink } from "./NavLink";
+export { PictureCarousel } from "./PictureCarousel";
+export { PromoCounter } from "./PromoCounter";
+export { UserProfileCard } from "./UserProfileCard";
+export { UserProfileHeader } from "./UserProfileHeader";
+export { default as VideoPlayer } from "./VideoPlayer";
---
+++ features/profile/ui/components/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Barrel exports for profile UI components
+ */
+
+export { ProfilePanel } from "./ProfilePanel";
